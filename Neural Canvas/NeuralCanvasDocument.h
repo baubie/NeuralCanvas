5bcfaa5d19f6ffb355de2053db0ff3b40616cfa6
Brandon Aubie <brandon@aubie.ca>
2011-08-13 11:08:09 -0400

    Initial Commit

---
+++ Neural Canvas/NeuralCanvasDocument.h
@@ -0,0 +1,14 @@
+//
+//  NeuralCanvasDocument.h
+//  Neural Canvas
+//
+//  Created by Brandon Aubie on 11-08-13.
+//  Copyright 2011 McMaster University. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface NeuralCanvasDocument : NSDocument {
+}
+
+@end
---
+++ Neural Canvas/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Neural Canvas
+//
+//  Created by Brandon Aubie on 11-08-13.
+//  Copyright 2011 McMaster University. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
